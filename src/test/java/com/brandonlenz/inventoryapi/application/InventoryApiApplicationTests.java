9efb5e4e489e2a5a64b4989a46e054a9913c75ce
Brandon Lenz <brandon@truss.works>
2021-06-10 12:24:47 -0400

    Comment @SpringBootTest
    - Prevent Spring (and therefore liquibase) from running somewhat unexpectedly

--- src/test/java/com/brandonlenz/inventoryapi/application/InventoryApiApplicationTests.java
+++ src/test/java/com/brandonlenz/inventoryapi/application/InventoryApiApplicationTests.java
@@ -3,11 +3,11 @@ package com.brandonlenz.inventoryapi.application;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class InventoryApiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class InventoryApiApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
